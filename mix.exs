fa8a5758e5770b6524b821364b67ebd8bcee2cec
speeddragon <speeddragon@gmail.com>
2019-02-04 19:09:18 +0000

    Bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CircleciOrb.MixProject do
   def project do
     [
       app: :circleci_orb,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
